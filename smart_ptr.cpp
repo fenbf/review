274971d1431ed482f8282895dbe9f5551e55282e
Bartłomiej Filipek <bartlomiej.filipek@gmail.com>
2014-08-27 12:51:06 +0200

    spaces instead of tabs

--- smart_ptr.cpp
+++ smart_ptr.cpp
@@ -5,11 +5,11 @@
 class Test
 {
 public:
-	Test() :m_value(0) { std::cout << "Test::Test" << std::endl; }
-	Test(const Test &t) :m_value(0) { std::cout << "Test::Test copy" << std::endl; }
-	~Test() { std::cout << "Test::~Test destructor" << std::endl; }
+    Test() :m_value(0) { std::cout << "Test::Test" << std::endl; }
+    Test(const Test &t) :m_value(0) { std::cout << "Test::Test copy" << std::endl; }
+    ~Test() { std::cout << "Test::~Test destructor" << std::endl; }
 
-	int m_value;
+    int m_value;
 };
 
 typedef std::auto_ptr<Test> TestAutoPtr;
@@ -27,19 +27,19 @@ void SharedPtrCastTest();
 
 void main()
 {
-	//AutoPtrTest();
+    //AutoPtrTest();
 
-	//UniquePtrTest();
+    //UniquePtrTest();
 
-	//UniquePtrArrayTest();
+    //UniquePtrArrayTest();
 
-	//SharedPtrTest();
+    //SharedPtrTest();
 
-	//SharedPtrArrayTest();
+    //SharedPtrArrayTest();
 
-	//SharedPtrParamTest();
+    //SharedPtrParamTest();
 
-	SharedPtrCastTest();
+    SharedPtrCastTest();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -47,15 +47,15 @@ void main()
 
 void doSomethig(TestAutoPtr myPtr)
 {
-	myPtr->m_value = 11;
+    myPtr->m_value = 11;
 }
 
 void AutoPtrTest()
 {
-	TestAutoPtr myTest(new Test());
-	doSomethig(myTest);
+    TestAutoPtr myTest(new Test());
+    doSomethig(myTest);
 
-	myTest->m_value = 10;
+    myTest->m_value = 10;
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -63,15 +63,15 @@ void AutoPtrTest()
 
 void doSomethig(TestUniquePtr myPtr)
 {
-	myPtr->m_value = 11;
+    myPtr->m_value = 11;
 }
 
 void UniquePtrTest()
 {
-	TestUniquePtr myTest(new Test());
-	doSomethig(std::move(myTest));
+    TestUniquePtr myTest(new Test());
+    doSomethig(std::move(myTest));
 
-	// myTest->m_value = 10;	// runtime error, but this time on purpose!
+    // myTest->m_value = 10;	// runtime error, but this time on purpose!
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -79,10 +79,10 @@ void UniquePtrTest()
 
 void UniquePtrArrayTest()
 {
-	// posiible, but generates runtime error
-	// std::unique_ptr<int> p(new int[10]); // only delete will be called
+    // posiible, but generates runtime error
+    // std::unique_ptr<int> p(new int[10]); // only delete will be called
 
-	std::unique_ptr<Test[]> tests(new Test[3]);
+    std::unique_ptr<Test[]> tests(new Test[3]);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -90,9 +90,9 @@ void UniquePtrArrayTest()
 
 void SharedPtrTest()
 {
-	std::shared_ptr<Test> sp(new Test());
+    std::shared_ptr<Test> sp(new Test());
 
-	std::shared_ptr<Test> sp2 = std::make_shared<Test>();
+    std::shared_ptr<Test> sp2 = std::make_shared<Test>();
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -100,7 +100,7 @@ void SharedPtrTest()
 
 void SharedPtrArrayTest()
 {
-	std::shared_ptr<Test> sp(new Test[2], [](Test *p) { delete[] p; });
+    std::shared_ptr<Test> sp(new Test[2], [](Test *p) { delete[] p; });
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -108,20 +108,20 @@ void SharedPtrArrayTest()
 
 void testSharedFunc(std::shared_ptr<Test> sp)
 {
-	sp->m_value = 10;
+    sp->m_value = 10;
 }
 
 void testSharedFuncRef(const std::shared_ptr<Test> &sp)
 {
-	sp->m_value = 10;
+    sp->m_value = 10;
 }
 
 void SharedPtrParamTest()
 {
-	std::shared_ptr<Test> sp = std::make_shared<Test>();
+    std::shared_ptr<Test> sp = std::make_shared<Test>();
 
-	testSharedFunc(sp);
-	testSharedFuncRef(sp);
+    testSharedFunc(sp);
+    testSharedFuncRef(sp);
 }
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -130,52 +130,52 @@ void SharedPtrParamTest()
 class BaseA
 {
 protected:
-	int a{ 0 };
+    int a{ 0 };
 public:
-	virtual ~BaseA() { }
+    virtual ~BaseA() { }
 
-	void A(int p) { a = p; }
+    void A(int p) { a = p; }
 };
 
 class ChildB : public BaseA
 {
 private:
-	int b{ 0 };
+    int b{ 0 };
 public:
-	void B(int p) { b = p; }
+    void B(int p) { b = p; }
 };
 
 class BadChild
 {
 private:
-	int c{ 0 };
+    int c{ 0 };
 public:
-	void C(int p) { c = p; }
+    void C(int p) { c = p; }
 };
 
 void SharedPtrCastTest()
 {
-	std::shared_ptr<BaseA> ptrBase = std::make_shared<ChildB>();
-	ptrBase->A(10);
-	std::cout << "use count A: " << ptrBase.use_count() << std::endl;
-	
-	// manual:
-	ChildB *ptrMan = dynamic_cast<ChildB *>(ptrBase.get());
-	ptrMan->B(10);
+    std::shared_ptr<BaseA> ptrBase = std::make_shared<ChildB>();
+    ptrBase->A(10);
+    std::cout << "use count A: " << ptrBase.use_count() << std::endl;
 
-	// ok:
-	std::shared_ptr<ChildB> ptrChild = std::dynamic_pointer_cast<ChildB>(ptrBase);
-	if (ptrChild)
-	{
-		ptrChild->B(20);
-		std::cout << "use count A: " << ptrBase.use_count() << std::endl;
-		std::cout << "use count B: " << ptrChild.use_count() << std::endl;
-	}	
+    // manual:
+    ChildB *ptrMan = dynamic_cast<ChildB *>(ptrBase.get());
+    ptrMan->B(10);
 
-	// bad:
-	std::shared_ptr<BadChild> ptrBad = std::dynamic_pointer_cast<BadChild>(ptrBase);
-	if (!ptrBad)
-	{
-		std::cout << "problem with cast!" << std::endl;
-	}
+    // ok:
+    std::shared_ptr<ChildB> ptrChild = std::dynamic_pointer_cast<ChildB>(ptrBase);
+    if (ptrChild)
+    {
+        ptrChild->B(20);
+        std::cout << "use count A: " << ptrBase.use_count() << std::endl;
+        std::cout << "use count B: " << ptrChild.use_count() << std::endl;
+    }
+
+    // bad:
+    std::shared_ptr<BadChild> ptrBad = std::dynamic_pointer_cast<BadChild>(ptrBase);
+    if (!ptrBad)
+    {
+        std::cout << "problem with cast!" << std::endl;
+    }
 }
