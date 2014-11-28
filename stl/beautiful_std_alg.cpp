1e1b8362500f8ed42a2678a68550614a5bd9ae55
Bartłomiej Filipek <bartlomiej.filipek@gmail.com>
2014-11-28 21:34:08 +0100

    - vc solution/project  - gather code with example  - better example for slide  - using cpp11 random, random_shuffle is deprecated in cpp11

--- stl/beautiful_std_alg.cpp
+++ stl/beautiful_std_alg.cpp
@@ -9,6 +9,8 @@
 #include <locale> // isspace
 #include <numeric> // iota
 #include <vector>
+#include <random>
+//#include <functional>   // std::not1
 
 #ifndef __func__
 	#define __func__ __FUNCTION__
@@ -30,10 +32,19 @@ template <typename T> void printAll(T start, T end, const std::string &title = "
 	std::cout << std::endl;
 }
 
+//
+// 5: random shuffle
+// 
 template <typename T, typename V> void generateRand(T start, T end, V startValue)
 {
 	std::iota(start, end, startValue);
-	std::random_shuffle(start, end);
+	
+	// deprecated in C++14:
+	// std::random_shuffle(start, end);
+	
+	std::random_device rd;
+    std::mt19937 gen(rd());
+	std::shuffle(start, end, gen);
 }
 
 //
@@ -60,6 +71,7 @@ void insertionSortTest()
 //
 // 2. quick sort made with std::algorithm building blocks
 //
+// source: http://stackoverflow.com/a/24650627/486617 "Algorithmic building blocks"
 template<class FwdIt, class Compare = std::less<>>
 void quickSort(FwdIt first, FwdIt last, Compare cmp = Compare{})
 {
@@ -88,6 +100,8 @@ void quickSortTest()
 //
 // 3. slide from Cpp Seasoning
 //
+// use case: list of items, make continous selection and move that selection into a new 'p' position.
+//
 // bug in GCC std: https://gcc.gnu.org/ml/gcc-help/2013-10/msg00000.html
 // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=58357
 // http://stackoverflow.com/questions/23993333/stdrotate-return-value-in-gcc-4-9
@@ -101,7 +115,7 @@ void slideTest()
 #else
 template <typename randIter> auto slide(randIter f, randIter l, randIter p) -> std::pair<randIter, randIter>
 {
-	if (p < f) return { p, std::rotate(p, f, l) ); 
+	if (p < f) return { p, std::rotate(p, f, l) };
 	if (l < p) return { std::rotate(f, l, p), p };
 	return { f, l };
 }
@@ -110,15 +124,47 @@ void slideTest()
 {
 	PRINT_TEST_TITLE();
 
-	std::vector<double> dvec(10);
-	std::iota(std::begin(dvec), std::end(dvec), -10.0);
-	printAll(std::begin(dvec), std::end(dvec), "initial");
+	int intArray[10] = { 0 };
+	intArray[2] = intArray[3] = intArray[4] = 1;
+	printAll(std::begin(intArray), std::end(intArray), "initial");
 
-	auto p = slide(std::begin(dvec), std::next(std::begin(dvec), 3), std::end(dvec));
-	printAll(std::begin(dvec), std::end(dvec), "+ 3 right");
-	std::cout << *(p.first) << std::endl;
+	auto p = slide(intArray + 2, intArray + 5, intArray + 7);
+	printAll(std::begin(intArray), std::end(intArray), "to id 6");
+	std::cout << "new range from " << std::distance(intArray, p.first) << " to " << std::distance(intArray, p.second) << " position" << std::endl;
 }
 #endif
+
+//
+// 5. gather (cpp seasoning)
+//
+// use case: list of items, select some of items (good guys) and move the to position around p.
+// for instance: multiple selection on a list
+//
+// problem with std::not1: http://channel9.msdn.com/Events/GoingNative/2013/Cpp-Seasoning#c635149692925101916
+
+template <typename I, // I models BidirectionalIterator
+	typename S> // S models UnaryPredicate
+	auto gather(I f, I l, I p, S s) -> std::pair < I, I >
+{
+	using value_type = typename std::iterator_traits<I>::value_type;
+	return{ std::stable_partition(f, p, [&](const value_type& x){ return !s(x); }),
+			std::stable_partition(p, l, s) };
+}
+
+void gatherTest()
+{
+	PRINT_TEST_TITLE();
+	
+	int intArray[10] = { 0 };
+	intArray[0] = intArray[2] = intArray[7] = intArray[8] = 1;
+	printAll(intArray, intArray + 10, "before");
+	
+	// gather all '1's and move them around 4th place in the array
+	auto range = gather(intArray, intArray + 10, intArray + 4, [](int x) { return x == 1; });
+	printAll(intArray, intArray + 10, "after");
+
+	std::cout << "selected items from " << std::distance(intArray, range.first) << " to " << std::distance(intArray, range.second) << " position" << std::endl;
+}
 
 //
 // 4. trim for strings
@@ -132,8 +178,8 @@ std::string trimLeft(const std::string &s) {
 #ifdef __GNUC__
 std::string trimRight(const std::string &s) {
 	auto temp = s;
-	auto tempRbeg = temp.rbegin();
-	auto tempRend = temp.rend();
+	const auto tempRbeg = temp.rbegin();
+	const auto tempRend = temp.rend();
 	temp.erase(std::find_if(tempRbeg, tempRend, [](char c){return !std::isspace(c, std::locale()); }).base(), std::end(temp));
 	return temp;
 }
@@ -188,6 +234,8 @@ int main()
 	
 	// 2:
 	slideTest();
+	
+	gatherTest();
 	
 	// 3:
 	quickSortTest();
